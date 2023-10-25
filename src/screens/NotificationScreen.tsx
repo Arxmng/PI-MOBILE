62ca939e1ea133f06305d10a0087dfa06a34ac75
Marcelo <27177081+Smart624@users.noreply.github.com>
2023-10-24 23:45:48 -0300

    added the queue and notification screens

---
+++ src/screens/NotificationScreen.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+const NotificationScreen: React.FC = () => {
+    return (
+        <View style={styles.container}>
+            <Image
+                style={styles.image}
+                source={require('../assets/notification.png')} // Adicione uma imagem de notificação aqui
+            />
+            <Text style={styles.title}>Atenção</Text>
+            <Text style={styles.message}>Sua posição na fila chegou, venha até o equipamento.</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fafafa',
+    },
+    image: {
+        width: 100,
+        height: 100,
+        marginBottom: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#333',
+    },
+    message: {
+        marginTop: 20,
+        fontSize: 18,
+        textAlign: 'center',
+        paddingHorizontal: 40,
+        color: '#666',
+    },
+});
+
+export default NotificationScreen;
